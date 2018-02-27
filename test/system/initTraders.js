ac06c5a79a9888ac3c68c4bc52cf7032a077dc24
guyon <guyon@live.com>
2018-02-27 20:13:10 +0000

    Major Refactoring

--- test/system/initTraders.js
+++
@@ -1,8 +0,0 @@
-const initTraders = (trader1, trader2) => {
-  return trader1.init_state(trader1.config)
-    .then(() => trader2.init_state(trader2.config))
-    .catch(error => {
-      console.error(error.stack);
-    });
-};
-module.exports = initTraders;
--- test/system/scenarios/spread-and-cancel.js
+++ test/system/scenarios/spread-and-cancel.js
@@ -1,23 +1,26 @@
+const Promish = require('promish');
+
 const run = (trader1) => {
   console.log('****************************************************************');
   console.log('** Scenario 1: Spread and Cancel                              **');
   console.log('****************************************************************');
-  return trader1.init_state(trader1.config)
+  return Promish.resolve()
     .then(() => trader1.placeLimitOrder('B', 2, 0.10))
     .then(() => trader1.placeLimitOrder('S', 1, 0.13))
     .then(() => trader1.placeLimitOrder('B', 1, 0.10))
     .then(() => trader1.placeLimitOrder('S', 1, 0.13))
-    .then(() => trader1.getMyOrders())
-    .then(() => trader1.cancelOrderById(trader1.myorders[0].orderId))
-    .then(() => trader1.cancelOrderById(trader1.myorders[1].orderId))
-    .delay(3000)
-    .then(() => trader1.getMyOrders())
+    .then(() => trader1.waitFor(() => trader1.orders.length === 4, 'Trader1 has 4 orders'))
+    .then(() => trader1.cancelOrderById(trader1.orders[0].orderId))
+    .then(() => trader1.waitFor(() => !trader1.orders[0].isOpen, 'Order[0] is closed'))
+    .then(() => trader1.cancelOrderById(trader1.orders[1].orderId))
+    .then(() => trader1.waitFor(() => !trader1.orders[1].isOpen, 'Order[1] is closed'))
+    .then(() => trader1.cancelOrderById(trader1.orders[2].orderId))
+    .then(() => trader1.waitFor(() => !trader1.orders[2].isOpen, 'Order[2] is closed'))
     .then(() => trader1.cancelAllOrders())
-    .then(() => trader1.getCurrentBalance())
-    .catch(error => {
-      console.error(error.stack);
-    });
+    .then(() => trader1.waitFor(() => !trader1.orders[3].isOpen, 'Order[3] is closed'))
+    .then(() => trader1.getCurrentBalance());
 };
+
 module.exports = {
   index: 1,
   run,
