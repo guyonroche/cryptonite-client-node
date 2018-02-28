919052382317395bc20949654d167ef62f44440b
guyon <guyon@live.com>
2018-02-28 08:07:04 +0000

    Fixing Stop order test

--- test/system/scenarios/market-stop-orders.js
+++ test/system/scenarios/market-stop-orders.js
@@ -6,14 +6,15 @@ const run = (trader1, trader2, trader3) => {
   console.log('****************************************************************');
 
   return Promish.resolve()
-    .then(() => trader1.placeLimitOrderSpread(0.21, 1, 3, 0.01))
+    .then(() => trader1.placeLimitOrderSpread(0.25, 1, 5, 0.01))
     .then(() => trader2.placeStopOrder('B', 0.25, 0.265))
     .then(() => trader2.placeStopOrder('S', 1, 0.235))
-    .then(() => trader3.placeMarketOrder('B', 0.25))
+    .then(() => trader3.placeMarketOrder('B', 1))
     .then(() => trader3.placeMarketOrder('S', 4))
     .then(() => trader2.getMyOrders())
     .then(() => trader1.cancelAllOrders());
 };
+
 module.exports = {
   index: 4,
   run,
