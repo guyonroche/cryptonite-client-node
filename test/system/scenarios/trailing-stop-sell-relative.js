cd7c64513842f9ccf5d5f4be8d8cbba31f3d0265
guyon <guyon@live.com>
2018-03-03 10:03:56 +0000

    Switching sides and a few other fixes

--- test/system/scenarios/trailing-stop-sell-relative.js
+++ test/system/scenarios/trailing-stop-sell-relative.js
@@ -1,6 +1,6 @@
 const run = (trader1, trader2, trader3) => {
   console.log('****************************************************************');
-  console.log('** Scenario 5: Trailing Stop Buy Relative                             **');
+  console.log('** Scenario 5: Trailing Stop Buy Relative                     **');
   console.log('****************************************************************');
 
   return  trader1.placeLimitOrderSpread(0.25, 0.5, 5, 0.01)
@@ -43,8 +43,8 @@ const run = (trader1, trader2, trader3) => {
 
     .then(() => trader1.cancelAllOrders())
     .then(() => trader2.cancelAllOrders());
-
 };
+
 module.exports = {
   index: 5,
   run,
