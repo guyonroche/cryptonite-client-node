6f6ca05ee8e8808af0d91517ba9a10884bf31257
anjnaharsora <anjnaharsora@gmail.com>
2018-03-05 10:27:28 +0530

    ST-3 added scenario to test trailing-stop-sell-offset

--- test/system/scenarios/trailing-stop-sell-offset.js
+++ test/system/scenarios/trailing-stop-sell-offset.js
@@ -1,14 +1,14 @@
 const run = (trader1, trader2, trader3) => {
   console.log('****************************************************************');
-  console.log('** Scenario 5: Trailing Stop Sell Relative                     **');
+  console.log('** Scenario 7: Trailing Stop Sell Offset                     **');
   console.log('****************************************************************');
 
   return  trader1.placeLimitOrderSpread(0.25, 0.5, 5, 0.01)
-    // make some trades to set the market price
+  // make some trades to set the market price
     .then(() => trader3.placeLimitOrder('S', 1, 0.24))
     .then(() => trader1.placeLimitOrder('B', 1, 0.24))
 
-    .then(() => trader2.placeTrailingStopOrder('S', 0.5, 'R', 0.05))
+    .then(() => trader2.placeTrailingStopOrder('S', 0.5, 'O', 0.15))
 
     .then(() => trader3.placeLimitOrder('B', 1, 0.26))
     .then(() => trader1.waitFor(() => trader1.hasMatchingTrade(0.5, 0.26), 'Trader 1 receive a trade of 0.5 LTC at 0.26'))
@@ -50,6 +50,6 @@ const run = (trader1, trader2, trader3) => {
 };
 
 module.exports = {
-  index: 5,
+  index: 7,
   run,
 };
